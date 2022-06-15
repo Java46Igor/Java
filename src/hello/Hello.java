1d357adbc628e14b59657c472460f39962f903ca
Leon363 <tuea1982@gmail.com>
2022-06-15 21:49:12 +0300

    SYSO * 3

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("????!Hello WORLD no ferx=ss sign again but this a ?!!!!!!!111??");
+		System.out.println(4*4);
+		System.out.print("jnbvnv");
 
 	}
 
