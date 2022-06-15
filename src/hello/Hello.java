a3f247a821f8b175b9b5ac0e07cff406591cf55d
Leon363 <tuea1982@gmail.com>
2022-06-15 20:46:00 +0300

    !+?

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("!!!!Hello WORLD no sign again but this a ???");
+		System.out.println("????!Hello WORLD no ferx=ss sign again but this a ?!!!!!!!111??");
 
 	}
 
